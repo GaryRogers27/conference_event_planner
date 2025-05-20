d798244f16c6680a80c4ed795d4352cb366b2317
GaryRogers27 <mr.rogers27@yahoo.com>
2025-05-20 13:37:10 -0400

    finished add on section, updated calc, added buttons

--- src/ConferenceEvent.jsx
+++ src/ConferenceEvent.jsx
@@ -3,11 +3,13 @@ import "./ConferenceEvent.css";
 import TotalCost from "./TotalCost";
 import { useSelector, useDispatch } from "react-redux";
 import { incrementQuantity, decrementQuantity } from "./venueSlice";
+import { incrementAvQuantity, decrementAvQuantity } from "./avSlice";
 
 const ConferenceEvent = () => {
     const [showItems, setShowItems] = useState(false);
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const venueItems = useSelector((state) => state.venue);
+        const avItems = useSelector((state) => state.av); 
     const dispatch = useDispatch();
     const remainingAuditoriumQuantity = 3 - venueItems.find(item => item.name === "Auditorium Hall (Capacity:200)").quantity;
 
@@ -30,10 +32,12 @@ const ConferenceEvent = () => {
         }
       };
     const handleIncrementAvQuantity = (index) => {
-    };
+    dispatch(incrementAvQuantity(index));
+        };
 
     const handleDecrementAvQuantity = (index) => {
-    };
+    dispatch(decrementAvQuantity(index));
+        };
 
     const handleMealSelection = (index) => {
        
@@ -54,9 +58,15 @@ const ConferenceEvent = () => {
           venueItems.forEach((item) => {
             totalCost += item.cost * item.quantity;
           });
+        } else if (section == "av") {
+            avItems.forEach((item) => {
+                totalCost += item.cost * item.quantity;
+            });
         }
         return totalCost;
       };
+    
+    const avTotalCost = calculateTotalCost("av");
     const venueTotalCost = calculateTotalCost("venue");
 
     const navigateToProducts = (idType) => {
@@ -160,7 +170,7 @@ const ConferenceEvent = () => {
                                 <div className="addons_selection">
 
                                 </div>
-                                <div className="total_cost">Total Cost:</div>
+                                <div className="total_cost">Total Cost: {avTotalCost}</div>
 
                             </div>
 
@@ -190,6 +200,21 @@ const ConferenceEvent = () => {
                         </div>
                     )
                 }
+
+                {avItems.map((item, index) => (
+                    <div className="av_data venue_main" key={index}>
+                         <div className="img">
+                         <img src={item.img} alt={item.name} />
+                     </div>
+                    <div className="text"> {item.name} </div>
+                         <div> ${item.cost} </div>
+                             <div className="addons_btn">
+                                <button className="btn-warning" onClick={() => handleDecrementAvQuantity(index)}> &ndash; </button>
+                                <span className="quantity-value">{item.quantity}</span>
+                                 <button className=" btn-success" onClick={() => handleIncrementAvQuantity(index)}> &#43; </button>
+                    </div>
+    </div>
+))}
 
 
 
